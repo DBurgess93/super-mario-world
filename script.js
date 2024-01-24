510211bad72fc7d0901a275e6420e5b7554d54bd
Daniel Burgess <dan-burgess@hotmail.com>
2024-01-24 13:13:24 +1030

    complex polylines - mapping out a track

--- script.js
+++ script.js
@@ -1,82 +1,107 @@
+let poly
+let map
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: { lat: -34.90294429051486, lng: 138.7020212174254 },
     zoom: 18,
     mapId: '2e40f4c01b12cb97',
-    mapTypeControl: false,
-    fullScreenControl: false,
-    streetViewControl: false,
+    // mapTypeControl: false,
+    // fullScreenControl: false,
+    // streetViewControl: false,
   });
 
-  const runPathcoordinates = [
-    { lat: -34.90294429051486, lng: 138.7020212174254 },
-    { lat: -34.90339260817647, lng: 138.70585938930034 },
-    { lat: -34.90392677070563, lng: 138.70383562594807 },
-    { lat: -34.90294429051486, lng: 138.7020212174254 }
-  ]
-
-  const runPath = new google.maps.Polyline({
-    path: runPathcoordinates,
-    geodesic: true,
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
-  runPath.setMap(map);
-
-
-  // -34.90294429051486, 138.7020212174254
-  // -34.90339260817647, 138.70585938930034
-  // -34.90392677070563, 138.70383562594807
-
-  // const markers = [
-  //   [
-  //     "Castle",
-  //     -34.90294429051486,
-  //     138.7020212174254,
-  //     "castle.svg",
-  //     38,
-  //     31,
-  //     1000
-  //   ],
-  //   [
-  //     "Ghost House",
-  //     -34.90339260817647,
-  //     138.70585938930034,
-  //     "ghosthouse.svg",
-  //     38,
-  //     31,
-  //     2000
-  //   ],
-  //   [
-  //     "Weird Hill",
-  //     -34.90392677070563,
-  //     138.70383562594807,
-  //     "hills_with_eyes.svg",
-  //     38,
-  //     31,
-  //     3000
-  //   ]
-  //   [
-  //     "Pointy Boi",
-  //     34.668562013820164,
-  //     135.43130168128872,
-  //     "pointer.svg",
-  //     38,
-  //     31,
-  //     4000
-  //   ],
-  //   [
-  //     "Star lol",
-  //     34.668454379776044,
-  //     135.43036814960806,
-  //     "star.svg",
-  //     38,
-  //     31,
-  //     5000
-  //   ],
-  // ];
+  poly = new google.maps.Polyline({
+    strokecolor: "#000000",
+    strokOpacity: 1.0,
+    strokeWeight: 3,
+  })
+  poly.setMap(map)
+  map.addListener("click", addLatLng)
 };
+
+function addLatLng(event) {
+  const path = poly.getPath();
+  path.push(event.latLng);
+
+  new google.maps.Marker({
+    position: event.latLng,
+    title: "#" + path.getLength(),
+    map: map,
+  });
+}
+
+window.initMap = initMap;
+
+
+// const runPathcoordinates = [
+//   { lat: -34.90294429051486, lng: 138.7020212174254 },
+//   { lat: -34.90339260817647, lng: 138.70585938930034 },
+//   { lat: -34.90392677070563, lng: 138.70383562594807 },
+//   { lat: -34.90294429051486, lng: 138.7020212174254 }
+// ]
+
+// const runPath = new google.maps.Polyline({
+//   path: runPathcoordinates,
+//   geodesic: true,
+//   strokeColor: "#FF0000",
+//   strokeOpacity: 1.0,
+//   strokeWeight: 2,
+// });
+// runPath.setMap(map);
+
+
+// -34.90294429051486, 138.7020212174254
+// -34.90339260817647, 138.70585938930034
+// -34.90392677070563, 138.70383562594807
+
+// const markers = [
+//   [
+//     "Castle",
+//     -34.90294429051486,
+//     138.7020212174254,
+//     "castle.svg",
+//     38,
+//     31,
+//     1000
+//   ],
+//   [
+//     "Ghost House",
+//     -34.90339260817647,
+//     138.70585938930034,
+//     "ghosthouse.svg",
+//     38,
+//     31,
+//     2000
+//   ],
+//   [
+//     "Weird Hill",
+//     -34.90392677070563,
+//     138.70383562594807,
+//     "hills_with_eyes.svg",
+//     38,
+//     31,
+//     3000
+//   ]
+//   [
+//     "Pointy Boi",
+//     34.668562013820164,
+//     135.43130168128872,
+//     "pointer.svg",
+//     38,
+//     31,
+//     4000
+//   ],
+//   [
+//     "Star lol",
+//     34.668454379776044,
+//     135.43036814960806,
+//     "star.svg",
+//     38,
+//     31,
+//     5000
+//   ],
+// ];
 
 for (let i = 0; i < markers.length; i++) {
   const currMarker = markers[i];
