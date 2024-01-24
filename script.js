10c98b62025336eea8c4737820afafb66ee3acf5
Daniel Burgess <dan-burgess@hotmail.com>
2024-01-24 19:30:55 +1030

    touch ups

--- script.js
+++ script.js
@@ -11,27 +11,57 @@ function initMap() {
     // streetViewControl: false,
   });
 
-  poly = new google.maps.Polyline({
-    strokecolor: "#000000",
-    strokOpacity: 1.0,
-    strokeWeight: 3,
-  })
-  poly.setMap(map)
-  map.addListener("click", addLatLng)
-};
-
-function addLatLng(event) {
-  const path = poly.getPath();
-  path.push(event.latLng);
-
-  new google.maps.Marker({
-    position: event.latLng,
-    title: "#" + path.getLength(),
-    map: map,
+  const drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [
+        google.maps.drawing.OverlayType.MARKER,
+        google.maps.drawing.OverlayType.CIRCLE,
+        google.maps.drawing.OverlayType.POLYGON,
+        google.maps.drawing.OverlayType.POLYLINE,
+        google.maps.drawing.OverlayType.RECTANGLE,
+      ],
+    },
+    markerOptions: {
+      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
+      animation: google.maps.Animation.DROP
+    },
+    circleOptions: {
+      fillColor: "#ffff00",
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1,
+    },
   });
-}
 
+  drawingManager.setMap(map);
+}
 window.initMap = initMap;
+
+//   poly = new google.maps.Polyline({
+//     strokecolor: "#000000",
+//     strokOpacity: 1.0,
+//     strokeWeight: 3,
+//   })
+//   poly.setMap(map)
+//   map.addListener("click", addLatLng)
+// };
+
+// function addLatLng(event) {
+//   const path = poly.getPath();
+//   path.push(event.latLng);
+
+//   new google.maps.Marker({
+//     position: event.latLng,
+//     title: "#" + path.getLength(),
+//     map: map,
+//   });
+// }
+
 
 
 // const runPathcoordinates = [
