c61dfa459fd327f1964b5de3a24df5e1ce93697e
Daniel Burgess <dan-burgess@hotmail.com>
2024-01-26 16:36:08 +1030

    measuring distance using polylines

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 let poly;
 let map;
-
-let markers = [];
+let polylines = [];
 let totalDistance = 0;
-
 const distancesContainer = document.getElementById('distances');
 const totalDistanceContainer = document.getElementById('totalDistances');
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.90294429051486, lng: 138.7020212174254 },
+async function initMap() {
+  const { Map, MVCArray } = await google.maps.importLibrary("maps")
+  map = new Map(document.getElementById('map'), {
+    center: { lat: -34.92889390968822, lng: 138.49628233355094 },
     zoom: 14,
     mapId: '2e40f4c01b12cb97',
   });
 
   const drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingMode: google.maps.drawing.OverlayType.POLYLINE,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
       drawingModes: [
+        google.maps.drawing.OverlayType.POLYLINE,
         google.maps.drawing.OverlayType.MARKER,
         google.maps.drawing.OverlayType.CIRCLE,
         google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.POLYLINE,
         google.maps.drawing.OverlayType.RECTANGLE,
       ],
     },
@@ -43,11 +42,35 @@ function initMap() {
 
   drawingManager.setMap(map);
 
+  let currentPolyline;
+
   google.maps.event.addListener(drawingManager, 'overlaycomplete', function (event) {
-    if (event.type === google.maps.drawing.OverlayType.MARKER) {
-      markers.push(event.overlay);
+    if (event.type === google.maps.drawing.OverlayType.POLYLINE) {
+      const polyline = event.overlay;
+      polylines.push(polyline);
+
+      const path = polyline.getPath();
+
+      google.maps.event.addListener(path, 'insert_at', function (index) {
+        console.log(`Point inserted at index ${index}`)
+        updateDistances()
+      })
+      google.maps.event.addListener(path, 'set_at', function (index) {
+        console.log(`Point set at index ${index}`)
+        updateDistances()
+      })
 
       updateDistances();
+
+      currentPolyline = new google.maps.Polyline({
+        map: map,
+        path: new google.maps.MVCArray()
+      })
+
+      google.maps.event.addListener(map, 'click', function (e) {
+        currentPolyline.getPath().push(e.latLng)
+        updateDistances()
+      })
     }
   });
 }
@@ -56,137 +79,28 @@ function updateDistances() {
   distancesContainer.innerHTML = '';
   totalDistance = 0;
 
-  for (let i = 1; i < markers.length; i++) {
-    const previousMarker = markers[i - 1];
-    const currentMarker = markers[i];
+  console.log("update distances triggered")
 
-    const distance = google.maps.geometry.spherical.computeDistanceBetween(
-      previousMarker.getPosition(),
-      currentMarker.getPosition()
-    );
+  for (let i = 0; i < polylines.length; i++) {
+    const polyline = polylines[i];
+    const path = polyline.getPath();
 
-    distancesContainer.innerHTML += `Distance ${i}-${i + 1}: ${distance.toFixed(2)} meters<br>`;
+    console.log(`Path ${i}:`, path.getArray().map(point => ({ lat: point.lat(), lng: point.lng() })));
 
-    totalDistance += distance;
+    for (let j = 1; j < path.getLength(); j++) {
+      const previousPoint = path.getAt(j - 1);
+      const currentPoint = path.getAt(j);
+
+      const distance = google.maps.geometry.spherical.computeDistanceBetween(
+        previousPoint,
+        currentPoint
+      );
+
+      distancesContainer.innerHTML += `Segment ${j}: ${distance.toFixed(2)} meters<br>`;
+      totalDistance += distance;
+    }
   }
 
   totalDistanceContainer.innerHTML = `Total Distance: ${totalDistance.toFixed(2)} meters`;
 }
-
 window.initMap = initMap;
-
-
-//   poly = new google.maps.Polyline({
-//     strokecolor: "#000000",
-//     strokOpacity: 1.0,
-//     strokeWeight: 3,
-//   })
-//   poly.setMap(map)
-//   map.addListener("click", addLatLng)
-// };
-
-// function addLatLng(event) {
-//   const path = poly.getPath();
-//   path.push(event.latLng);
-
-//   new google.maps.Marker({
-//     position: event.latLng,
-//     title: "#" + path.getLength(),
-//     map: map,
-//   });
-// }
-
-
-
-// const runPathcoordinates = [
-//   { lat: -34.90294429051486, lng: 138.7020212174254 },
-//   { lat: -34.90339260817647, lng: 138.70585938930034 },
-//   { lat: -34.90392677070563, lng: 138.70383562594807 },
-//   { lat: -34.90294429051486, lng: 138.7020212174254 }
-// ]
-
-// const runPath = new google.maps.Polyline({
-//   path: runPathcoordinates,
-//   geodesic: true,
-//   strokeColor: "#FF0000",
-//   strokeOpacity: 1.0,
-//   strokeWeight: 2,
-// });
-// runPath.setMap(map);
-
-
-// -34.90294429051486, 138.7020212174254
-// -34.90339260817647, 138.70585938930034
-// -34.90392677070563, 138.70383562594807
-
-// const markers = [
-//   [
-//     "Castle",
-//     -34.90294429051486,
-//     138.7020212174254,
-//     "castle.svg",
-//     38,
-//     31,
-//     1000
-//   ],
-//   [
-//     "Ghost House",
-//     -34.90339260817647,
-//     138.70585938930034,
-//     "ghosthouse.svg",
-//     38,
-//     31,
-//     2000
-//   ],
-//   [
-//     "Weird Hill",
-//     -34.90392677070563,
-//     138.70383562594807,
-//     "hills_with_eyes.svg",
-//     38,
-//     31,
-//     3000
-//   ]
-//   [
-//     "Pointy Boi",
-//     34.668562013820164,
-//     135.43130168128872,
-//     "pointer.svg",
-//     38,
-//     31,
-//     4000
-//   ],
-//   [
-//     "Star lol",
-//     34.668454379776044,
-//     135.43036814960806,
-//     "star.svg",
-//     38,
-//     31,
-//     5000
-//   ],
-// ];
-
-// for (let i = 0; i < markers.length; i++) {
-//   const currMarker = markers[i];
-//   // const markerDrop = [1000, 2000, 3000, 4000, 5000, 6000]
-
-//   const marker = new google.maps.Marker({
-//     position: { lat: currMarker[1], lng: currMarker[2] },
-//     map,
-//     title: currMarker[0],
-//     icon: {
-//       url: currMarker[3],
-//       scaledSize: new google.maps.Size(currMarker[4], currMarker[5])
-//     },
-//     animation: google.maps.Animation.DROP,
-//   });
-
-//   const infowindow = new google.maps.InfoWindow({
-//     content: currMarker[0],
-//   })
-
-//   marker.addListener("click", () => {
-//     infowindow.open(map, marker);
-//   });
-// }
