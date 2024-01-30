ba6390c4b196ee32b06bb2de0d4d6d223bae1620
Daniel Burgess <dan-burgess@hotmail.com>
2024-01-30 19:46:27 +1030

    add markers

--- script.js
+++ script.js
@@ -4,45 +4,31 @@ let polylines = [];
 let totalDistance = 0;
 const distancesContainer = document.getElementById('distances');
 const totalDistanceContainer = document.getElementById('totalDistances');
+let drawingManager;
+let currentPolyline;
+// let isDrawing = false;
 
 async function initMap() {
-  const { Map, MVCArray } = await google.maps.importLibrary("maps")
+  const { Map, MVCArray } = await google.maps.importLibrary('maps');
   map = new Map(document.getElementById('map'), {
     center: { lat: -34.92889390968822, lng: 138.49628233355094 },
     zoom: 14,
     mapId: '2e40f4c01b12cb97',
   });
 
-  const drawingManager = new google.maps.drawing.DrawingManager({
+  drawingManager = new google.maps.drawing.DrawingManager({
     drawingMode: google.maps.drawing.OverlayType.POLYLINE,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: [
-        google.maps.drawing.OverlayType.POLYLINE,
-        google.maps.drawing.OverlayType.MARKER,
-        google.maps.drawing.OverlayType.CIRCLE,
-        google.maps.drawing.OverlayType.POLYGON,
-        google.maps.drawing.OverlayType.RECTANGLE,
-      ],
+      drawingModes: [google.maps.drawing.OverlayType.POLYLINE],
     },
-    markerOptions: {
-      icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-      animation: google.maps.Animation.DROP
-    },
-    circleOptions: {
-      fillColor: "#ffff00",
-      fillOpacity: 1,
-      strokeWeight: 5,
-      clickable: false,
+    polylineOptions: {
       editable: true,
-      zIndex: 1,
     },
   });
 
   drawingManager.setMap(map);
-
-  let currentPolyline;
 
   google.maps.event.addListener(drawingManager, 'overlaycomplete', function (event) {
     if (event.type === google.maps.drawing.OverlayType.POLYLINE) {
@@ -52,25 +38,39 @@ async function initMap() {
       const path = polyline.getPath();
 
       google.maps.event.addListener(path, 'insert_at', function (index) {
-        console.log(`Point inserted at index ${index}`)
-        updateDistances()
-      })
+        console.log(`Point inserted at index ${index}`);
+        updateDistances();
+      });
       google.maps.event.addListener(path, 'set_at', function (index) {
-        console.log(`Point set at index ${index}`)
-        updateDistances()
-      })
+        console.log(`Point set at index ${index}`);
+        updateDistances();
+      });
 
       updateDistances();
 
+      currentPolyline = null;
+      isDrawing = false;
+      drawingManager.setDrawingMode(null);
+    }
+  });
+
+  google.maps.event.addListener(map, 'mousedown', function (e) {
+    if (!isDrawing) {
       currentPolyline = new google.maps.Polyline({
         map: map,
-        path: new google.maps.MVCArray()
-      })
+        path: new google.maps.MVCArray(),
+        editable: false,
+      });
+      isDrawing = true;
+      drawingManager.setDrawingMode(google.maps.drawing.OverlayType.POLYLINE);
+    }
+  });
 
-      google.maps.event.addListener(map, 'click', function (e) {
-        currentPolyline.getPath().push(e.latLng)
-        updateDistances()
-      })
+  google.maps.event.addListener(map, 'mousemove', function (e) {
+    if (isDrawing) {
+      const path = currentPolyline.getPath();
+      path.push(e.latLng);
+      updateDistances();
     }
   });
 }
@@ -79,22 +79,15 @@ function updateDistances() {
   distancesContainer.innerHTML = '';
   totalDistance = 0;
 
-  console.log("update distances triggered")
-
   for (let i = 0; i < polylines.length; i++) {
     const polyline = polylines[i];
     const path = polyline.getPath();
-
-    console.log(`Path ${i}:`, path.getArray().map(point => ({ lat: point.lat(), lng: point.lng() })));
 
     for (let j = 1; j < path.getLength(); j++) {
       const previousPoint = path.getAt(j - 1);
       const currentPoint = path.getAt(j);
 
-      const distance = google.maps.geometry.spherical.computeDistanceBetween(
-        previousPoint,
-        currentPoint
-      );
+      const distance = google.maps.geometry.spherical.computeDistanceBetween(previousPoint, currentPoint);
 
       distancesContainer.innerHTML += `Segment ${j}: ${distance.toFixed(2)} meters<br>`;
       totalDistance += distance;
@@ -103,4 +96,5 @@ function updateDistances() {
 
   totalDistanceContainer.innerHTML = `Total Distance: ${totalDistance.toFixed(2)} meters`;
 }
+
 window.initMap = initMap;
